5a32097e77edc381d02f95fed3f4ae4dfb777a40
Art <artem.nii@gmail.com>
2023-03-13 00:49:41 +0300

    task 9 10 done

--- cypress/e2e/apiEkzzz.cy.js
+++ cypress/e2e/apiEkzzz.cy.js
@@ -12,7 +12,7 @@ let name = faker.internet.userName()
 
 describe('Api final dz', () => {
 
-  it.skip('Task 1. Get all posts. Verify HTTP response status code and content type.', () => {
+  it('Task 1. Get all posts. Verify HTTP response status code and content type.', () => {
     cy.request('GET', '/posts')
       .then(response => {
         expect(response.headers['content-type']).to.have.string(contentType)
@@ -20,7 +20,7 @@ describe('Api final dz', () => {
       })
   })
 
-  it.skip('Task 2. Get only first 10 posts. Verify HTTP response status code. Verify that only first posts are returned', () => {
+  it('Task 2. Get only first 10 posts. Verify HTTP response status code. Verify that only first posts are returned', () => {
     cy.request('GET', 'posts?_limit=10')
       .then(response => {
         expect((response.body).length).to.equal(10)
@@ -29,7 +29,7 @@ describe('Api final dz', () => {
       })
   })
 
-  it.skip('Task 3. Get posts with id = 5 and id = 11. Verify HTTP response status code. Verify id values of returned records', () => {
+  it('Task 3. Get posts with id = 5 and id = 11. Verify HTTP response status code. Verify id values of returned records', () => {
     cy.request('GET', 'posts?id=5&id=11')
       .then(response => {
         expect(response.status).to.be.equal(200)
@@ -38,7 +38,7 @@ describe('Api final dz', () => {
       })
   })
 
-  it.skip('Task 4. Create a post. Verify HTTP response status code 401', () => {
+  it('Task 4. Create a post. Verify HTTP response status code 401', () => {
     cy.request({
       method: 'POST',
       url: '664/posts',
@@ -49,7 +49,7 @@ describe('Api final dz', () => {
       })
   })
 
-  it.skip('Task 5. Create post with adding access token in header. Verify HTTP response status code. Verify post is created1', () => {
+  it('Task 5. Create post with adding access token in header. Verify HTTP response status code. Verify post is created1', () => {
     cy.request({
       method: 'POST',
       url: 'register',
@@ -72,12 +72,12 @@ describe('Api final dz', () => {
     })
   })
 
-  it.skip('Task 6. Create post entity and verify that the entity is created. Verify HTTP response status code. Use JSON in body', () => {
+  it('Task 6. Create post entity and verify that the entity is created. Verify HTTP response status code. Use JSON in body', () => {
     cy.request({
       method: 'POST',
       url: 'register',
       body: {
-        "email": email,
+        "email": `${email}x`,
         "password": "bestPassw0rd"
       }
     }).then(response => {
@@ -98,7 +98,7 @@ describe('Api final dz', () => {
     })
   })
 
-  it.skip('Task 7. Update non-existing entity. Verify HTTP response status code', () => {
+  it('Task 7. Update non-existing entity. Verify HTTP response status code', () => {
     cy.request({
       method: 'PUT',
       url: 'posts/789',
@@ -118,7 +118,7 @@ describe('Api final dz', () => {
       method: 'POST',
       url: 'register',
       body: {
-        "email": email,
+        "email": `${email}z`,
         "password": "bestPassw0rd"
       }
     }).then(response => {
@@ -140,9 +140,6 @@ describe('Api final dz', () => {
             method: 'PUT',
             url: `posts/${response.body.id}`,
             failOnStatusCode: false,
-            headers: {
-              "Authorization": `Bearer ${response.body.accessToken}`
-            },
             body: {
               "qa": "art after"
             }
@@ -155,20 +152,75 @@ describe('Api final dz', () => {
     })
   })
 
-
-
-
-
-  it.skip('Task 9. Delete non-existing post entity. Verify HTTP response status code', () => {
-    cy.request('POST', '/664/posts') //201
+  it('Task 9. Delete non-existing post entity. Verify HTTP response status code', () => {
+    cy.request({
+      method: 'DELETE',
+      url: 'posts/789',
+      failOnStatusCode: false
+    })
+      .then(response => {
+        expect(response.status).to.be.equal(404)
+      })
   })
 
-  it.skip('Task 10. Create post entity, update the created entity, and delete the entity. Verify HTTP response status code and verify that the entity is deleted', () => {
-    cy.request('POST', '/664/posts') //201
+
+
+
+  it('Task 10. Create post entity, update the created entity, and delete the entity. Verify HTTP response status code and verify that the entity is deleted', () => {
+
+    cy.request({
+      method: 'POST',
+      url: 'register',
+      body: {
+        "email": `${email}c`,
+        "password": "bestPassw0rd"
+      }
+    }).then(response => {
+      cy.request({
+        method: 'POST',
+        url: '664/posts',
+        headers: {
+          "Authorization": `Bearer ${response.body.accessToken}`
+        },
+        body: {
+          "qa": "art before"
+        }
+      })
+        .then(response => {
+          expect(response.status).to.be.equal(201)
+          expect(response.body.qa).to.be.equal('art before')
+
+          cy.request({
+            method: 'PUT',
+            url: `posts/${response.body.id}`,
+            failOnStatusCode: false,
+            body: {
+              "qa": "art after"
+            }
+          })
+            .then(response1 => {
+              expect(response1.status).to.be.equal(200)
+              expect(response1.body.qa).to.be.equal('art after')
+
+              cy.request({
+                method: 'DELETE',
+                url: `posts/${response1.body.id}`,
+                failOnStatusCode: false
+              })
+                .then(response => {
+                  expect(response.status).to.be.equal(200)
+
+                  cy.request({
+                    method: 'GET',
+                    url: `posts/${response1.body.id}`,
+                    failOnStatusCode: false
+                  })
+                    .then(response => {
+                      expect(response.status).to.be.equal(404)
+                    })
+                })
+            })
+        })
+    })
   })
-
-
-
-
-
 })
