83f45fbe5f9a84ad27ffd7b535e7bc30cd434c5b
Art <artem.nii@gmail.com>
2023-03-12 22:12:33 +0300

    task 1 2 3 done

---
+++ cypress/e2e/apiEkzzz.cy.js
@@ -0,0 +1,73 @@
+///<reference types="cypress"/>
+
+import { faker } from "@faker-js/faker"
+
+let contentType = 'application/json; charset=utf-8'
+
+describe('Api final dz', () => {
+  it.skip('Task 1. Get all posts. Verify HTTP response status code and content type.', () => {
+    cy.request('GET', '/posts')
+      .then(response => {
+        expect(response.headers['content-type']).to.have.string(contentType)
+        expect(response.isOkStatusCode).to.be.true
+      })
+  })
+
+  it.skip('Task 2. Get only first 10 posts. Verify HTTP response status code. Verify that only first posts are returned', () => {
+    cy.request('GET', 'posts?_limit=10')
+    .then(response => {
+      expect((response.body).length).to.equal(10)
+      expect(response.body[0].title).to.have.string('json-server 1')
+      expect(response.body[9].title).to.have.string('json-server 10')
+    })
+
+  })
+
+
+
+
+
+
+  it('Task 3. Get posts with id = 5 and id = 11. Verify HTTP response status code. Verify id values of returned records', () => {
+    cy.request('GET', 'posts?id=5&id=11')
+    .then(response => {
+      expect(response.status).to.be.equal(200)
+      expect(response.body[0].id).to.eq(5)
+      expect(response.body[1].id).to.eq(11)
+    })
+
+  })
+
+  it.skip('Task 4. Create a post. Verify HTTP response status code 401', () => {
+    cy.request('POST', '/664/posts')
+  })
+
+  it.skip('Task 5. Create post with adding access token in header. Verify HTTP response status code. Verify post is created1', () => {
+    cy.request('POST', '/664/posts') //201
+  })
+
+  it.skip('Task 6. Create post entity and verify that the entity is created. Verify HTTP response status code. Use JSON in body', () => {
+    cy.request('POST', '/664/posts') //201
+  })
+
+  it.skip('Task 7. Update non-existing entity. Verify HTTP response status code', () => {
+    cy.request('POST', '/664/posts') //201
+  })
+
+  it.skip('Task 8. Create post entity and update the created entity. Verify HTTP response status code and verify that the entity is updated', () => {
+    cy.request('POST', '/664/posts') //201
+  })
+
+  it.skip('Task 9. Delete non-existing post entity. Verify HTTP response status code', () => {
+    cy.request('POST', '/664/posts') //201
+  })
+
+  it.skip('Task 10. Create post entity, update the created entity, and delete the entity. Verify HTTP response status code and verify that the entity is deleted', () => {
+    cy.request('POST', '/664/posts') //201
+  })
+
+
+
+
+
+})
